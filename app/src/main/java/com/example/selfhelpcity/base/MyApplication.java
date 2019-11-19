2b92577c8b5b2016535cb314c50d6a54a21bec7a
CYH06 <451644091@qq.com>
2019-11-19 18:47:07 +0800

    11-18  11点54

--- app/src/main/java/com/example/selfhelpcity/base/MyApplication.java
+++ app/src/main/java/com/example/selfhelpcity/base/MyApplication.java
@@ -7,12 +7,21 @@ import android.util.Log;
 
 import com.example.selfhelpcity.BuildConfig;
 import com.example.selfhelpcity.model.ObjectBox;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheEntity;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.cookie.CookieJarImpl;
+import com.lzy.okgo.cookie.store.DBCookieStore;
+import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 import io.objectbox.android.AndroidObjectBrowser;
 import me.jessyan.autosize.AutoSizeConfig;
 import me.jessyan.autosize.unit.Subunits;
+import okhttp3.OkHttpClient;
 
 import static com.example.selfhelpcity.base.Constant.PATH_IPFS_DOWNLOAD;
 import static com.example.selfhelpcity.base.Constant.PATH_IPFS_USB;
@@ -38,10 +47,34 @@ public class MyApplication extends Application {
         initAutoSize();
         initObjectBox();
         initFolder();
+        initOkGo();
     }
 
+    private void initOkGo() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //log相关
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkGo");
+        loggingInterceptor.setPrintLevel(HttpLoggingInterceptor.Level.BODY);        //log打印级别，决定了log显示的详细程度
+        loggingInterceptor.setColorLevel(Level.INFO);                               //log颜色级别，决定了log在控制台显示的颜色
+        builder.addInterceptor(loggingInterceptor);
+        //超时时间设置，默认60秒
+        builder.readTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);      //全局的读取超时时间
+        builder.writeTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);     //全局的写入超时时间
+        builder.connectTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);   //全局的连接超时时间
 
+//        /自动管理cookie（或者叫session的保持），以下几种任选其一就行
+        //builder.cookieJar(new CookieJarImpl(new SPCookieStore(this)));            //使用sp保持cookie，如果cookie不过期，则一直有效
+        builder.cookieJar(new CookieJarImpl(new DBCookieStore(this)));              //使用数据库保持cookie，如果cookie不过期，则一直有效
+        //builder.cookieJar(new CookieJarImpl(new MemoryCookieStore()));            //使用内存保持cookie，app退出后，cookie消失
+// 其他统一的配置
+        // 详细说明看GitHub文档：https://github.com/jeasonlzy/
+        OkGo.getInstance().init(this)                           //必须调用初始化
+                .setOkHttpClient(builder.build())               //建议设置OkHttpClient，不设置会使用默认的
+                .setCacheMode(CacheMode.NO_CACHE)               //全局统一缓存模式，默认不使用缓存，可以不传
+                .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)   //全局统一缓存时间，默认永不过期，可以不传
+                .setRetryCount(3);                           //全局统一超时重连次数，默认为三次，那么最差的情况会请求4次(一次原始请求，三次重连请求)，不需要可以设置为0
 
+    }
 
 
     private void initFolder() {
@@ -67,7 +100,7 @@ public class MyApplication extends Application {
         ObjectBox.init(this);
         if (BuildConfig.DEBUG) {
             boolean started = new AndroidObjectBrowser(ObjectBox.get()).start(this);
-            Log.i("ObjectBrowser","ObjectBrowser Started: " + started);
+            Log.i("ObjectBrowser", "ObjectBrowser Started: " + started);
         }
     }
 
@@ -77,6 +110,7 @@ public class MyApplication extends Application {
                 .setSupportSP(true)
                 .setSupportSubunits(Subunits.NONE);
     }
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
--- app/src/main/java/com/example/selfhelpcity/view/SplashActivity.java
+++ app/src/main/java/com/example/selfhelpcity/view/SplashActivity.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
 import com.comenjoysoft.baselibrary.util.SPUtils;
 import com.example.selfhelpcity.R;
+import com.example.selfhelpcity.base.Api;
 import com.example.selfhelpcity.base.BaseActivity;
 import com.example.selfhelpcity.base.Constant;
 import com.example.selfhelpcity.services.KeepLiveService;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.Callback;
+import com.lzy.okgo.model.Progress;
+import com.lzy.okgo.model.Response;
+import com.lzy.okgo.request.base.Request;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.runtime.Permission;
+
+import org.json.JSONArray;
 
 /**
  * 欢迎页
@@ -33,16 +42,16 @@ public class SplashActivity extends BaseActivity {
                     new Handler() {
                         @Override
                         public void handleMessage(Message msg) {
-                            if ( SPUtils.getInstance(SplashActivity.this).getBoolean(Constant.SP_KEY_FIRST_LAUNCH, true) ) {
+                            if (SPUtils.getInstance(SplashActivity.this).getBoolean(Constant.SP_KEY_FIRST_LAUNCH, true)) {
                                 SPUtils.getInstance(SplashActivity.this).put(Constant.SP_KEY_FIRST_LAUNCH, false);
                                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                                 finish();
                                 overridePendingTransition(R.anim.enter_alpha, R.anim.exit_alpha);
                             } else {
                                 String password = SPUtils.getInstance(getApplicationContext()).getString(Constant.SP_KEY_WALLET_ADDRESS, "");
-                                if ( !password.isEmpty() ) {
+                                if (!password.isEmpty()) {
                                     startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                                   finish();
+                                    finish();
                                 } else {
                                     startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                                     finish();
@@ -57,6 +66,8 @@ public class SplashActivity extends BaseActivity {
 
                 })
                 .start();
+
+
     }
 
     @Override
@@ -66,7 +77,47 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        OkGo.<JSONArray>get(Api.GET_PEOPLE_LIST).tag(this).execute(new Callback<JSONArray>() {
+            @Override
+            public void onStart(Request<JSONArray, ? extends Request> request) {
 
+            }
+
+            @Override
+            public void onSuccess(Response<JSONArray> response) {
+                Log.d("ljyljy", "onSuccess: "+response.body());
+            }
+
+            @Override
+            public void onCacheSuccess(Response<JSONArray> response) {
+
+            }
+
+            @Override
+            public void onError(Response<JSONArray> response) {
+                Log.d("ljyljy", "onError: "+response.code());
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void uploadProgress(Progress progress) {
+
+            }
+
+            @Override
+            public void downloadProgress(Progress progress) {
+
+            }
+
+            @Override
+            public JSONArray convertResponse(okhttp3.Response response) throws Throwable {
+                return null;
+            }
+        });
     }
 
     @Override
